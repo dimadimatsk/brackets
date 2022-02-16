20f7e5f07f950eb9633158e1a5507bf9fb8ef43a
Dmitriy Shilovskiy <dimareectsk@gmail.com>
2022-02-16 13:51:54 +0700

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,37 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  let BRACKETS_PAIR = {};
+  bracketsConfig.forEach((element) => {
+    BRACKETS_PAIR[element[0]] = element[1];
+  });
+
+  let stack = [];
+  let pairStack = [];
+
+  for (let elem of str) {
+    if (
+      BRACKETS_PAIR[elem] === elem &&
+      pairStack[pairStack.length - 1] === elem
+    ) {
+      if (elem === stack[stack.length - 1]) {
+        stack.pop();
+        pairStack.pop();
+      } else return false;
+    } else if (
+      BRACKETS_PAIR[elem] === elem &&
+      pairStack[pairStack.length - 1] !== elem
+    ) {
+      stack.push(BRACKETS_PAIR[elem]);
+      pairStack.push(BRACKETS_PAIR[elem]);
+    } else if (
+      Object.values(BRACKETS_PAIR).includes(elem) &&
+      BRACKETS_PAIR[elem] !== elem
+    ) {
+      if (elem === stack[stack.length - 1]) stack.pop();
+      else return false;
+    } else {
+      stack.push(BRACKETS_PAIR[elem]);
+    }
+  }
+
+  return stack.length === 0;
+};
